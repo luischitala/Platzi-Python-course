0f8704cd4965cef5f2b3db3fa9cbb35b48bfcb3f
luischitala <lrchitala@gmail.com>
2020-01-15 17:53:22 -0600

    Binary search, scopte of functions and docstrings

---
+++ docstrings.py
@@ -0,0 +1,8 @@
+def a(cualquier_parametro):
+    """Descripcion de lo que hace nuestra funcion
+       cualquier_parametro int cualquier entero
+       returns cualquier_parametro + 5"""
+
+
+    return cualquier_parametro + 5
+help(a)
